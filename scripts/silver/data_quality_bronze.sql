bb9ee4a7bad6484dbacb2fed01606bfbae90c84c
DominicSamoes <dominicsamo@outlook.com>
2026-02-01 16:34:21 +0200

    Silver layer DDL

---
+++ scripts/silver/data_quality_bronze.sql
@@ -0,0 +1,66 @@
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Result
+SELECT
+    cst_id,
+    COUNT(*) AS cnt
+FROM
+    bronze.crm_cust_info
+GROUP BY
+    cst_id
+HAVING
+    COUNT(*) > 1
+    OR cst_id IS NULL;
+
+-- Transform and Clean
+--Window Function to get the latest record based on cst_create_date
+SELECT
+    *,
+    ROW_NUMBER() OVER (
+        PARTITION BY
+            cst_id
+        ORDER BY
+            cst_create_date DESC
+    ) AS flag_last
+FROM
+    bronze.crm_cust_info
+WHERE
+    cst_id = 29466;
+
+-- Query by latest record only
+SELECT
+    *
+FROM
+    (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY
+                    cst_id
+                ORDER BY
+                    cst_create_date DESC
+            ) AS flag_last
+        FROM
+            bronze.crm_cust_info
+    )
+WHERE
+    flag_last = 1;
+
+-- Check for unwanted spaces
+-- Expectation: No Result
+SELECT
+    cst_lastname
+FROM
+    bronze.crm_cust_info
+WHERE
+    cst_lastname != TRIM(cst_lastname);
+
+-- Data Standardization & Consistency
+SELECT DISTINCT
+    cst_gndr
+FROM
+    bronze.crm_cust_info;
+
+SELECT DISTINCT
+    cst_marital_status
+FROM
+    bronze.crm_cust_info;
